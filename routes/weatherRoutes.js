8b42ac3836010677b6438445613f49f7b8eab9b3
Jacob Hildebrandt <jake@hildy.ca>
2021-05-11 16:50:37 -0700

    Get Data from Open Weather API

--- routes/weatherRoutes.js
+++ routes/weatherRoutes.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const axios = require("axios");
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env" });
+const API_KEY = process.env.WEATHER_API_KEY;
 
 const router = express.Router();
 
-router.route("/").get((req, res) => {
-  res.status(200).json({ status: "success", message: "weather EP" });
+router.route("/").get(async (req, res) => {
+  const city = req.query.city;
+  try {
+    const weather = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+    );
+    res.status(200).json({ status: "success", data: weather.data });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
